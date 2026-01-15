6a17f329b43753027dd2d8fd1013748bcfeea111
SeyedRumaiz <mrsrumaiz2007@gmail.com>
2026-01-15 21:13:45 +0530

    Added template component flow handler and organized folders

---
+++ dashboard/handlers/abstract_component_flow_handler.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractclassmethod
+from core.logger import Logger
+from typing import Any
+
+class AbstractComponentFlowHandler(ABC):
+    """
+    Abstract base class implementing the Template Method pattern.
+
+    Defines a fixed workflow for fault analysis while allowing
+    subclasses to implement individual steps.
+    """
+
+    def __init__(self) -> None:
+        self.logger = Logger.get_logger()
+        self.result = None
+
+    def start_flow(self, image_data=None, string_data=None):
+        """
+        Execute the analysis workflow.
+        
+        image_data : Any
+            Input image data.
+        string_data : Any
+            Input textual data
+        """
+        
+        self.logger.info("Starting %s", self.__class__.__name__)
+        self.pre_process_data(image_data, string_data)
+        self.apply_model()
+        self.present_results()
+        return self.result
+    
+    @abstractclassmethod
+    def pre_process_data(self, image_data: Any, string_data: Any) -> None:
+        """
+        Prepare data before analysis.
+        """
+
+    @abstractclassmethod
+    def apply_model(self) -> None:
+        """
+        Apply model logic
+        """
+
+    @abstractclassmethod
+    def present_results(self) -> None:
+        """
+        Present analysis results
+        """
