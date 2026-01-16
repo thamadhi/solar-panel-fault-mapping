61f1f5f72ffac436633bf43bcb9f9b01a1bad38e
dionrasmika16-web <dionrasmika16@gmail.com>
2026-01-16 15:42:05 +0530

    Add files via upload

---
+++ dashboard/user_option_handler.py
@@ -0,0 +1,23 @@
+from analysis_result import AnalysisResult
+from fault_detection_handler import FaultDetectionHandler
+from fault_localisation_handler import FaultLocalisationHandler
+from fault_severity_handler import FaultSeverityHandler
+from fault_rectification_handler import FaultRectificationHandler
+from report_handler import ReportHandler
+
+class UserOptionHandler:
+    def __init__(self):
+       
+        self.__DetectionResult = AnalysisResult()
+        self.__LocalisationResult = AnalysisResult()
+        self.__SeverityResult = AnalysisResult()
+        self.__RectificationResult = AnalysisResult()
+
+    def solarOperatorControl(self) -> None:
+        print("Operator Mode")
+
+    def maintenanceTechControl(self) -> None:
+        print("Tech Mode")
+
+    def fullControl(self) -> None:
+        print("Admin Mode")
