642a7e2fb36a82c08374304432f059e72170e535
SeyedRumaiz <mrsrumaiz2007@gmail.com>
2026-02-04 06:36:05 +0530

    Enhanced logger for file logs

--- dashboard/handlers/fault_localisation_handler.py
+++ dashboard/handlers/fault_localisation_handler.py
@@ -1,7 +1,7 @@
-from typing import Optional
+from typing import List
 
 from abstract_component_flow_handler import AbstractComponentFlowHandler
-from analysis_result import AnalysisResult
+from core.analysis_result import AnalysisResult
 
 
 class FaultLocalisatiobHandler(AbstractComponentFlowHandler):
