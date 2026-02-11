7f5ae9bb122c2e62466cab3aa5f49a5cece6e22c
SeyedRumaiz <mrsrumaiz2007@gmail.com>
2026-02-11 19:34:22 +0530

    WIP: Setup API

--- dashboard/api.py
+++ dashboard/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, json
 from core.logger import LoggerFactory
+from handlers.fault_detection_handler import FaultDetectionHandler
 
 # Configure logging once
 LoggerFactory.setup()
@@ -7,10 +8,23 @@ LoggerFactory.setup()
 # Create the flask app
 app = Flask(__name__)
 
+MODEL_PATH = "models/best_neural_network.keras"
+SCALER_PATH = "models/ann_scaler.pkl"
+
+# Load once
+handler = FaultDetectionHandler(
+    electrical_model_path=MODEL_PATH,
+    scaler_path=SCALER_PATH
+)
+
 # Define API endpoint
 @app.route("/predict", methods=["POST"])
 def predict():
-    pass
+    data = request.get_json()
+
+    # Expecitng features list
+    features = data['features']
+
 
 
 # Run the server
