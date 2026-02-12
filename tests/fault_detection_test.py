451e4d270557e9459fa4a795155b14fa8592e956
SeyedRumaiz <mrsrumaiz2007@gmail.com>
2026-02-12 15:26:12 +0530

    Added testing for feature names and preprocessing data

--- tests/fault_detection_test.py
+++ tests/fault_detection_test.py
@@ -15,6 +15,11 @@ from dashboard.handlers.fault_detection_handler import (
 from dashboard.core.logger import LoggerFactory
 import logging
 
+handler = FaultDetectionHandler(
+    electrical_model_path="dashboard/models/best_neural_network.keras",
+    scaler_path="dashboard/models/ann_scaler.pkl"
+)
+
 def test_pre_process_data():
     pass
 
@@ -28,10 +33,6 @@ def test_present_results():
 
 
 def test_hotspot():
-    handler = FaultDetectionHandler(
-        electrical_model_path="dashboard/models/best_neural_network.keras",
-        scaler_path="dashboard/models/ann_scaler.pkl"
-    )
     path = "dashboard/handlers/single.jpg"
 
     result = handler.start_flow(image_data=path)
@@ -46,3 +47,36 @@ def test_logger_setup_runs_once():
 
     logger = LoggerFactory.get_logger(__name__)
     assert isinstance(logger, logging.Logger)
+
+
+def test_preprocess_image_data_invalid_path():
+    result = handler._preprocess_image_data("non_existing.jpg")
+
+    assert result is None
+
+def test_apply_model_with_no_data():
+    handler.pre_process_data(None, None)
+    handler.apply_model()
+
+    assert handler.fault_type is None
+
+
+def test_feature_names():
+    features = handler.feature_names
+
+    feature_names = ['vdc1', 'vdc2', 'idc1', 'idc2',
+                        'irradiance', 'temperature',
+                        'power_string1', 'power_string2',
+                        'total_power', 
+                        'voltage_ratio', 'current_ratio']
+
+    assert isinstance(features, list)
+    assert all(f in feature_names for f in features)
+
+# Mock electrical ANN
+@patch("dashboard.handlers.fault_detection_handler.ElectricalANN")
+def test_apply_model_mock_ann(mock_ann):
+    mock_strategy = Mock()
+    pass
+
+
