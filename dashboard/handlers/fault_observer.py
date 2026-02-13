cbbef0582546a031c2669b198998ee53007611b5
SeyedRumaiz <mrsrumaiz2007@gmail.com>
2026-02-13 05:39:26 +0530

    Made .gitignore ignore models and MLflow, added session history

---
+++ dashboard/handlers/fault_observer.py
@@ -0,0 +1,11 @@
+# from core.fault import Fault
+# from abc import ABC, abstractmethod
+
+
+# class FaultObserver:
+#     @abstractmethod
+#     def update(self, fault: Fault):
+#         """
+#         Called when a fault is detected.
+#         """
+#         pass
