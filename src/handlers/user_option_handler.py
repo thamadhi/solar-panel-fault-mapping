893e1d576c53a5ef2379762317731ca65902bedc
SeyedRumaiz <mrsrumaiz2007@gmail.com>
2026-03-07 13:30:00 +0530

    Removed unnecessary conditional constraints from fault_detection_handler.py

--- src/handlers/user_option_handler.py
+++ src/handlers/user_option_handler.py
@@ -1,7 +1,7 @@
 from core.analysis_result import AnalysisResult
 from src.handlers.fault_detection_handler import FaultDetectionHandler
-from fault_localisation_handler import FaultLocalisationHandler
-from handlers.fault_Severity_handler import FaultSeverityHandler
+from src.handlers.fault_localisation_handler import FaultLocalisatiobHandler
+from src.handlers.fault_severity_handler import FaultSeverityHandler
 from fault_rectification_handler import FaultRectificationHandler
 from report_handler import ReportHandler
 
