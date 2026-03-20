920bf6b6aa22e6b042e058a43500ee1747421b83
Thamadhi <tamadhi.20241004@iit.ac.lk>
2026-03-20 05:52:27 +0530

    Add render css folder

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ init_state()
 render_page_config()
 render_css("assets/styles.css")
 render_css("assets/landing.css")
+render_css("assets/loc_tab.css")
 
 # App router to switch between tabs/pages
 app_router = AppRouter()
