40e4836c09f67e1b9f963e4d4e7a74df30ed69f7
SeyedRumaiz <mrsrumaiz2007@gmail.com>
2026-04-03 17:07:16 +0530

    Remove unncessary comments

--- src/handlers/fault_detection_handler.py
+++ src/handlers/fault_detection_handler.py
@@ -1,8 +1,5 @@
-# Standard libraries
 from typing import Any, Dict, List, Optional
 from typing_extensions import override
-
-# Local/project imports
 from src.handlers.handler import Handler
 from src.core.analysis_result import AnalysisResult
 from src.strategies.electrical_rf_strategy import ElectricalRF
